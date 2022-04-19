3cb35722ede00d8d1513f5e6859792273c2d3fec
Phil Bell <phil@legl.com>
2022-04-19 17:55:05 +0100

    fixing run and adding output

--- sort.py
+++ sort.py
@@ -6,4 +6,8 @@ import sublime_plugin
 class IsortFormat(sublime_plugin.EventListener):
     def on_post_save_async(self, view):
         if view.file_name().endswith(".py"):
-            subprocess.run(f"isort {view.file_name()}".split(), shell=True)
+            run = subprocess.run(
+                f"isort '{view.file_name()}'", shell=True, capture_output=True
+            )
+            print(run.stdout.decode("utf-8"))
+            print(run.stderr.decode("utf-8"))
